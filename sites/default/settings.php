025a7ff05842667a3e09cec0f9afa2b0cf8ca927
Edgar Barrantes <erian@suskind.local>
2015-09-08 16:27:49 -0600

    Modified settings.php for local database and drush use.

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -573,9 +573,9 @@ $conf['404_fast_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
  * valid settings.php file is present but no tables are installed, remove
  * the leading hash sign below.
  */
-$conf['pressflow_smart_start'] = TRUE;
+# $conf['pressflow_smart_start'] = TRUE;
 
-# Fix issue with recaptcha.
+# Try to fix recaptcha issue.
 ini_set('arg_separator.output', '&');
 
 if (file_exists(dirname(__FILE__) . '/settings.local.inc')) {
