daaedcba429aab51b521ec5b654ed0f9dacd3698
Edgar Barrantes <edgarbb@gmail.com>
2015-11-11 16:14:38 -0600

    Modifies a panel layout

--- sites/all/themes/auge-theme/panels/panels-one-col-wmargin/panels-one-col-wmargin.tpl.php
+++ sites/all/themes/auge-theme/panels/panels-one-col-wmargin/panels-one-col-wmargin.tpl.php
@@ -1,19 +1,27 @@
 <?php
 /**
- * @file
- * Template for a one column panel layout with a fixed width centered.
- *
- * This template provides a very simple "one column" panel display layout.
- *
- * Variables:
- * - $id: An optional CSS id to use for the layout.
- * - $content: An array of content, each item in the array is keyed to one
- *   panel of the layout. This layout supports the following sections:
- *   $content['content']: The only panel in the layout.
- */
+* @file
+* Template for a one column panel layout with a fixed width centered.
+*
+* This template provides a very simple "one column" panel display layout.
+*
+* Variables:
+* - $id: An optional CSS id to use for the layout.
+* - $content: An array of content, each item in the array is keyed to one
+*   panel of the layout. This layout supports the following sections:
+*   $content['content']: The only panel in the layout.
+*/
 ?>
-<div class="panel-display panel-display-margin panel-one-col-wmargin clearfix <?php if (!empty($css_id)): print $css_id; endif; ?>>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+<div class="panel-display panel-display-margin panel-one-col-wmargin panel-grid-width <?php if (!empty($css_id)): print $css_id; endif; ?>>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
   <div class="panel-panel panel-content panel-one-col-wmargin-content">
     <?php print $content['content']; ?>
+  </div>
+  <div class="panel-panel panel-content panel-one-col-wide">
+    <div class="panel-grid-width">
+      <?php print $content['content_wide']; ?>
+    </div>
+  </div>
+  <div class="panel-panel panel-content panel-one-col-wmargin-content panel-grid-width">
+    <?php print $content['content_bottom']; ?>
   </div>
 </div>
